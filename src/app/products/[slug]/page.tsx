589b69708fd02bdb3d75ed2b5b117d0d2084dcb7
박준서 <jswing5267@naver.com>
2023-10-29 17:03:16 +0900

    4.17강까지

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -7,11 +8,15 @@ type Props = {
 };
 
 export default function PostPage({ params }: Props) {
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+  return (
+    <>
+      <h1>{params.slug} 제품 설명 페이지</h1>
+    </>
+  );
 }
 
 export function generateStaticParams() {
-  const products = ["pants", "skirt"];
+  const products = ["pants", "skirt", "shirt", "shoes"];
   return products.map((product) => ({
     slug: product,
   }));
