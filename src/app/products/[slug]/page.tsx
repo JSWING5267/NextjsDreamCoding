38695e5bde35ddf6375945255579cba9a451b6f9
박준서 <jswing5267@naver.com>
2023-11-03 22:02:17 +0900

    5.8강까지

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import React from "react";
+import { getProduct, getProducts } from '@/service/products';
+import { notFound } from 'next/navigation';
 
 type Props = {
   params: {
@@ -9,21 +9,20 @@ type Props = {
 
 export function generateMetadata({ params }: Props) {
   return {
-    title: `제품의 이름:${params.slug}`,
+    title: `제품의 이름: ${params.slug}`,
   };
 }
 
-export default function PostPage({ params }: Props) {
-  return (
-    <>
-      <h1>{params.slug} 제품 설명 페이지</h1>
-    </>
-  );
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+
+  return <h1>{product?.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ["pants", "skirt", "shirt", "shoes"];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
