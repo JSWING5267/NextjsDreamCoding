f26e9545d0f93daa4f03937a847828943e266061
박준서 <jswing5267@naver.com>
2023-11-05 14:38:08 +0900

    6.10강: Redirect, Rewrite

---
+++ src/app/about/me/junseo/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>나는 준서임!</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import os from "os";
 import Counter from "@/components/Counter";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   console.log("안녕! - 서버");
