07095e8644197cd63c76d873ce7f75617105b200
박준서 <jswing5267@naver.com>
2023-11-10 19:23:00 +0900

    ver.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import os from "os";
 import Counter from "@/components/Counter";
 
-
 export default function Home() {
   console.log("안녕! - 서버");
   console.log(os.hostname());
   return (
     <>
-      <h1>홈페이지다!!</h1>
+      <h1>홈페이지다!! 버전2</h1>
       <Counter />
       <Image
         src={" https://images.unsplash.com/photo-1441986300917-64674bd600d8"}
